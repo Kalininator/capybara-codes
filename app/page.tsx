a9842a24d780bbc6bd10487b72455a64575d2ba2
Alexander Kalinin <alex@kalinin.uk>
2023-07-01 01:58:40 +0700

    highlight capy codes on home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
     <p>Capybara status codes.</p> 
-    <a href="/418">capybara.codes/418</a>
+    <a href="/200">capybara.codes/200</a>
     <p>Actual home page soon ™️</p> 
-    <p>Only have 418 picture for now. Rest coming soon</p>
+    <p>Only have 200/404 picture for now. Rest coming soon</p>
     </main>
   )
 }
