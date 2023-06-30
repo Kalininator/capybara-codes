fe8f2a60b17032694a96b3cc70f53c8cb4822d36
Alexander Kalinin <alex@kalinin.uk>
2023-07-01 01:49:59 +0700

    simple home page, 404 capy

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">Capybara status codes. <a href="/418">capybara.codes/418</a> Actual home page soon ™️ </main>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <p>Capybara status codes.</p> 
+    <a href="/418">capybara.codes/418</a>
+    <p>Actual home page soon ™️</p> 
+    <p>Only have 418 picture for now. Rest coming soon</p>
+    </main>
   )
 }
