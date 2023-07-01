37b3ad59968fb9a750fcb863f062e705d2d9862d
Alex Kalinin <alex@kalinin.uk>
2023-07-01 13:39:11 +0700

    4 gracefully handle invalid codes in path (#5)
    * redirect to home if status not found
    
    * Create not-found.tsx
    
    * stuff
    
    * meh
    
    * 🥴
    
    * Update not-found.tsx

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -1,11 +1,15 @@
 import { statuses } from "@/lib/status"
 import { Metadata } from "next"
+import { redirect } from 'next/navigation'
 import Image from "next/image"
 
 export async function generateMetadata(
   { params }: { params: { code: string } }
 ): Promise<Metadata> {
   const status = statuses[params.code];
+  if(status === undefined) {
+    return {};
+  }
  
   return {
     title: `Capybara Codes - ${status.code}`,
@@ -16,7 +20,6 @@ export async function generateMetadata(
 	height: 1400,
 	alt: `${status.code} - ${status.message}`,
       }
-
     },
     twitter: {
       card: 'summary_large_image',
@@ -31,7 +34,10 @@ export async function generateMetadata(
 }
 
 export default function CodePage({ params }: { params: { code: string } }) {
+  if(!Object.keys(statuses).includes(params.code)) {
+    redirect("/");
+  }
   return <div className="w-screen h-screen flex flex-col justify-center items-center">
     <Image src={`/${params.code}.jpg`} alt="Status code {code} image" width={800} height={700}/>
-    </div>
+  </div>
 }
---
+++ app/not-found.tsx
@@ -0,0 +1,47 @@
+// import { statuses } from "@/lib/status"
+import { Metadata } from "next"
+import Head from "next/head"
+import Image from "next/image"
+// const status = statuses["400"];
+
+// export const metadata: Metadata = {
+//     title: `Capybara Codes - ${status.code}`,
+//     openGraph: {
+//       images:       {
+// 	url: `/${status.code}.jpg`,
+// 	width: 1600,
+// 	height: 1400,
+// 	alt: `${status.code} - ${status.message}`,
+//       }
+//
+//     },
+//     twitter: {
+//       card: 'summary_large_image',
+//       title: `${status.code} - ${status.message}`,
+//       description: `${status.code} - ${status.message}`,
+//       // siteId: '1467726470533754880',
+//       // creator: '@nextjs',
+//       // creatorId: '1467726470533754880',
+//       images: [`/${status.code}.jpg`],
+//     },
+// }
+
+export default function NotFound() {
+  return (
+    <>
+      <head>
+	<meta property="og:image" content={`https://${process.env.VERCEL_URL || "localhost:3000"}/400.jpg`}/>
+	<meta property="og:image:width" content="1600"/>
+	<meta property="og:image:height" content="1400"/>
+	<meta property="og:image:alt" content="404 - Not found"/>
+	<meta name="twitter:card" content="summary_large_image"/>
+	<meta name="twitter:title" content="404 - Not found"/>
+	<meta name="twitter:description" content="404 - Not found"/>
+	<meta name="twitter:image" content={`https://${process.env.VERCEL_URL || "localhost:3000"}/400.jpg`}/>
+      </head>
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+	<Image src="/404.jpg" alt="Status code {code} image" width={800} height={700}/>
+      </div>
+    </>
+  )
+}
