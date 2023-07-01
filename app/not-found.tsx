a98a5417141c10c846fb064100530cf11f937616
Alexander Kalinin <alex@kalinin.uk>
2023-07-01 13:41:50 +0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -30,14 +30,14 @@ export default function NotFound() {
   return (
     <>
       <head>
-	<meta property="og:image" content={`https://${process.env.VERCEL_URL || "localhost:3000"}/400.jpg`}/>
+	<meta property="og:image" content={`https://${process.env.VERCEL_URL || "localhost:3000"}/404.jpg`}/>
 	<meta property="og:image:width" content="1600"/>
 	<meta property="og:image:height" content="1400"/>
 	<meta property="og:image:alt" content="404 - Not found"/>
 	<meta name="twitter:card" content="summary_large_image"/>
 	<meta name="twitter:title" content="404 - Not found"/>
 	<meta name="twitter:description" content="404 - Not found"/>
-	<meta name="twitter:image" content={`https://${process.env.VERCEL_URL || "localhost:3000"}/400.jpg`}/>
+	<meta name="twitter:image" content={`https://${process.env.VERCEL_URL || "localhost:3000"}/404.jpg`}/>
       </head>
       <div className="w-screen h-screen flex flex-col justify-center items-center">
 	<Image src="/404.jpg" alt="Status code {code} image" width={800} height={700}/>
