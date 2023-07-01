e58b1925f016cd62506b8ff11bfa7a35c9d25a1d
Alexander Kalinin <alex@kalinin.uk>
2023-07-01 17:50:32 +0700

    install prettier and run it

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 // import { statuses } from "@/lib/status"
-import { Metadata } from "next"
-import Head from "next/head"
-import Image from "next/image"
+import { Metadata } from "next";
+import Head from "next/head";
+import Image from "next/image";
 // const status = statuses["400"];
 
 // export const metadata: Metadata = {
@@ -30,18 +30,33 @@ export default function NotFound() {
   return (
     <>
       <head>
-	<meta property="og:image" content={`https://${process.env.VERCEL_URL || "localhost:3000"}/404.jpg`}/>
-	<meta property="og:image:width" content="1600"/>
-	<meta property="og:image:height" content="1400"/>
-	<meta property="og:image:alt" content="404 - Not found"/>
-	<meta name="twitter:card" content="summary_large_image"/>
-	<meta name="twitter:title" content="404 - Not found"/>
-	<meta name="twitter:description" content="404 - Not found"/>
-	<meta name="twitter:image" content={`https://${process.env.VERCEL_URL || "localhost:3000"}/404.jpg`}/>
+        <meta
+          property="og:image"
+          content={`https://${
+            process.env.VERCEL_URL || "localhost:3000"
+          }/404.jpg`}
+        />
+        <meta property="og:image:width" content="1600" />
+        <meta property="og:image:height" content="1400" />
+        <meta property="og:image:alt" content="404 - Not found" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="404 - Not found" />
+        <meta name="twitter:description" content="404 - Not found" />
+        <meta
+          name="twitter:image"
+          content={`https://${
+            process.env.VERCEL_URL || "localhost:3000"
+          }/404.jpg`}
+        />
       </head>
       <div className="w-screen h-screen flex flex-col justify-center items-center">
-	<Image src="/404.jpg" alt="Status code {code} image" width={800} height={700}/>
+        <Image
+          src="/404.jpg"
+          alt="Status code {code} image"
+          width={800}
+          height={700}
+        />
       </div>
     </>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-    <p>Capybara status codes.</p> 
-    <a href="/200">capybara.codes/200</a>
-    <p>Actual home page soon ™️</p> 
+      <p>Capybara status codes.</p>
+      <a href="/200">capybara.codes/200</a>
+      <p>Actual home page soon ™️</p>
     </main>
-  )
+  );
 }
