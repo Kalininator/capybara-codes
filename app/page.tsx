6cf54877d894606ebe4d3287bc962199a9a4dd48
Alex Kalinin <alex@kalinin.uk>
2023-07-02 10:58:48 +0700

    Better home styling (#15)
    * cleanup global css
    
    * better tile scaling for mobile/desktop
    
    * more tiles for xl

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen gap-6 flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen gap-6 flex-col items-center justify-center py-16 lg:px-24">
       <h1 className="text-4xl font-bold">Capybara Codes</h1>
       <p>A capybara for every HTTP status code</p>
       <div className="flex flex-row flex-wrap gap-4 justify-center items-center content-evenly">
         {Object.values(statuses).map((status) => (
-          <Link key={status.code} href={`/${status.code}`}>
+          <Link
+            key={status.code}
+            href={`/${status.code}`}
+            className="w-2/5 md:w-1/4 xl:w-1/5"
+          >
             <Image
               src={`/${status.code}.jpg`}
               alt={status.message}
