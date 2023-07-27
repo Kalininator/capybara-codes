06914374273bef8e7d43008f50a899cc1aad5ab6
Alexander Kalinin <alex@kalinin.uk>
2023-07-27 16:05:15 +0800

    try?

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -9,8 +9,8 @@ function Error({ statusCode }: any) {
 }
 
 Error.getInitialProps = ({ res, err }: any) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
+  // const statusCode = res ? res.statusCode : err ? err.statusCode : 200;
+  return { statusCode: 200 };
 };
 
 export default Error;
