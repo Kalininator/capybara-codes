c2c02c81a761c51827b31d753aaae9a9d4f8eecc
Alex Kalinin <alex@kalinin.uk>
2024-09-24 13:49:35 +0100

    sitemap and robots (#24)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
+  site: 'https://capy.codes',
+  integrations: [tailwind(), sitemap()],
+  output: "hybrid",
   adapter: vercel()
 });
